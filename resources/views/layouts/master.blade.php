34213e2244cfb91ccf7a97ddc75ecd7468f4e5ea
kazashim <kazashimkuzasuwat@gmail.com>
2020-01-08 01:31:14 -0800

    main updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
@@ -11,7 +10,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title>AdminLTE 3 | Starter</title>
-        <link rel="stylesheet" href="/css/app.css">
+
+  <link rel="stylesheet" href="/css/app.css">
+
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -39,7 +40,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- Right navbar links -->
 
-
   </nav>
   <!-- /.navbar -->
 
@@ -47,9 +47,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="./img/boy-1.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="./img/man.png" alt="Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">Multi Purp</span>
+      <span class="brand-text font-weight-light">Multi Purpos</span>
     </a>
 
     <!-- Sidebar -->
@@ -57,12 +57,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="./img/man.png" class="img-circle elevation-2" alt="User Image">
+          <img src="./img/boy-1.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{ Auth::user()->name }}
-
-          </a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
@@ -82,13 +80,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="#" class="nav-link active">
-                  <i class="far fas fa-circle nav-icon"></i>
+                  <i class="far fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
-                  <i class="far fas fa-circle nav-icon"></i>
+                  <i class="far fa-circle nav-icon"></i>
                   <p>Inactive Page</p>
                 </a>
               </li>
@@ -197,7 +195,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
   </div>
   <!-- /.content-wrapper -->
 
-
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
 
   <!-- Main Footer -->
   <footer class="main-footer">
@@ -211,6 +217,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 </div>
 <!-- ./wrapper -->
 
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
 <script src="/js/app.js"><script>
+    <script src="dist/js/adminlte.min.js"></script>
 </body>
 </html>
