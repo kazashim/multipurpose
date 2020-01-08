c9a181068badaa2e79e6fc9dc09d70e48dcad8ee
kazashim <kazashimkuzasuwat@gmail.com>
2020-01-08 01:14:03 -0800

    js update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -60,7 +60,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="./img/man.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}
+
+          </a>
         </div>
       </div>
 
