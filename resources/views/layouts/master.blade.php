5197e37c07d2fc9f2d4738032ae4d7487b75e825
kazashim <kazashimkuzasuwat@gmail.com>
2020-01-09 08:06:03 -0800

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -75,7 +75,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <p>
                     Dashboard
                   </p>
-                </rout>
+                </router-link>
               </li>
 
 
@@ -109,7 +109,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               <p>
                 Profile
               </p>
-            </a>
+            </router-link>
           </li>
 
 
