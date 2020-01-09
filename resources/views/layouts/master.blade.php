e78460c9036e8649a3cd00e3e86f26638aac76f2
kazashim <kazashimkuzasuwat@gmail.com>
2020-01-09 06:57:03 -0800

    master update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>AdminLTE 3 | Starter</title>
+  <title>CynMulti | Starter</title>
 
   <link rel="stylesheet" href="/css/app.css">
 
@@ -163,7 +163,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2019 <a href="https://cynojine.com">CynMulti</a>.</strong> All rights reserved.
   </footer>
 </div>
 <!-- ./wrapper -->
