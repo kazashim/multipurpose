b0aca4c72f883a5b5c11e01b99701f52564e6f22
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-01-10 04:20:53 -0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -52842,8 +52842,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\xampp\htdocs\multipurpose\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\xampp\htdocs\multipurpose\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\George\Documents\GitHub\multipurpose\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\George\Documents\GitHub\multipurpose\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
