5379ac9367ec7aaf96d39c91225b0ded251c8941
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-01-10 13:09:26 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('{path}','HomeController@index')->where( 'path', '([A-z\d-/_.]+)?' );
+Route::get('/dasboard','HomeController@index')->where( 'path', '([A-z\d-/_.]+)?' );
