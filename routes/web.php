52dadea51f938ccb10e2391cb040fb200e06d82b
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-01-10 13:19:26 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('./components/Dashboard.vue', 'HomeController@index')->name('dashboard');
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
