6028dfb3086e219cb55c44be18b20529648e2070
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-01-13 08:02:58 -0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 import { Form, HasError, AlartError } from 'vform';
 
-window.Form = Form
+window.Form = Form;
 Vue.component(HasError.name, HasError)
 Vue.component(AlertError.name, AlertError)
 
