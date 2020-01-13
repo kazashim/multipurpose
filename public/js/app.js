350aa42f1ddc0f88a82c020d2823a65894fe4eca
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-01-13 01:28:18 -0800

    s

--- public/js/app.js
+++ public/js/app.js
@@ -2126,7 +2126,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
-    return {};
+    return {
+      Form: new Form({})
+    };
   },
   mounted: function mounted() {
     console.log('Component mounted.');
