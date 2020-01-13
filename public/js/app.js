7db782b38c98c582f74217f29aae20f3ea6a2fdd
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-01-13 01:18:34 -0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -53900,6 +53900,9 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
 
+window.form = vform__WEBPACK_IMPORTED_MODULE_0__["Form"];
+Vue.component(vform__WEBPACK_IMPORTED_MODULE_0__["HasError"].name, vform__WEBPACK_IMPORTED_MODULE_0__["HasError"]);
+Vue.component(AlertError.name, AlertError);
 
 Vue.use(vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]);
 var routes = [{
